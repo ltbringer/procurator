412fa5ba2ec9f2753e16ff9055257db018dd67cc
AmreshVenugopal <amresh.venugopal@gmail.com>
2020-03-30 04:30:26 +0530

    add: functions to query tables

---
+++ procurator/db/__init__.py
@@ -0,0 +1,26 @@
+import os
+import psycopg2 as pg
+from procurator.config import DATABASE
+
+
+def get_connection():
+    config = DATABASE[os.environ["APP_ENV"]]
+    return pg.connect(host=config["host"], password=config["password"],
+                      port=config["port"], dbname=config["dbname"],
+                      user=config["user"])
+
+def get_user_nicks():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("select id, nickname from users")
+    return cursor.fetchall()
+
+def get_user_knowledge(user_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(f"""
+    select id, questions, answers from answers
+    where
+    submitted_by = {user_id}
+    """)
+    return cursor.fetchall()
