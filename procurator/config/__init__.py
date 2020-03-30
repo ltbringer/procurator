2901430e147557785aac067e1f538a35d1ff2a68
AmreshVenugopal <amresh.venugopal@gmail.com>
2020-03-30 13:15:16 +0530

    update: use env vars only, instead of separating dev config.

--- procurator/config/__init__.py
+++ procurator/config/__init__.py
@@ -1,15 +1,7 @@
 import os
 
 
-DB_DEV_CONFIG = {
-    "host": os.environ.get("DB_HOST", "0.0.0.0"),
-    "port": os.environ.get("DB_PORT", 7555),
-    "password": os.environ.get("DB_PASSWORD", "procurator"),
-    "dbname": os.environ.get("DB_NAME", "procurator"),
-    "user": os.environ.get("DB_USER", "procurator")
-}
-
-DB_PROD_CONFIG = {
+DATABASE = {
     "host": os.environ.get("DB_HOST"),
     "port": os.environ.get("DB_PORT"),
     "password": os.environ.get("DB_PASSWORD"),
@@ -17,8 +9,4 @@ DB_PROD_CONFIG = {
     "user": os.environ.get("DB_USER")
 }
 
-
-DATABASE = {
-    "dev": DB_DEV_CONFIG,
-    "prod": DB_PROD_CONFIG
-}
+print(DATABASE)
